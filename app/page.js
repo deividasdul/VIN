7cf75b7935f64586f29762d32d92254ef0c378bf
deividasdul <deividas.dul@gmail.com>
2025-02-12 23:05:20 +0200

    Almost done

--- app/page.js
+++ app/page.js
@@ -1,62 +1,232 @@
 "use client";
 
-import { Paper, TextField, Button, Stack } from "@mui/material";
+import {
+  Paper,
+  TextField,
+  Button,
+  Stack,
+  Box,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 
+const CustomTextField = ({ value, name, label, placeholder, onChange }) => {
+  return (
+    <TextField
+      value={value}
+      name={name}
+      label={label}
+      placeholder={placeholder}
+      minRows={10}
+      maxRows={15}
+      multiline
+      helperText="* Kiekvieną įrašą atskirkite paspausdami Enter."
+      required
+      onChange={onChange}
+      color="success"
+    ></TextField>
+  );
+};
+
+const CustomResultTextField = ({ label, value }) => {
+  return (
+    <TextField
+      fullWidth
+      label={label}
+      value={value}
+      minRows={10}
+      maxRows={15}
+      multiline
+      disabled
+    ></TextField>
+  );
+};
+
 export default function Home() {
-  const [vins, setVins] = useState([]);
-  const [result, setResult] = useState();
+  const [cars, setCars] = useState({
+    vin: "",
+    model: "",
+    delivery: "",
+    received: "",
+    status: "",
+  });
+
+  const [newCars, setNewCars] = useState({
+    vin: "",
+    model: "",
+    delivery: "",
+    received: "",
+    status: "",
+  });
+
+  const [carAmount, setCarAmount] = useState(0);
+  const [resultAmount, setResultAmount] = useState(0);
 
   const onChange = (e) => {
-    const { value } = e.target;
+    const { name, value } = e.target;
 
-    setVins(value);
+    setCars({
+      ...cars,
+      [name]: value,
+    });
+  };
+
+  const [result, setResult] = useState({
+    vin: "",
+    model: "",
+    delivery: "",
+    received: "",
+    status: "",
+  });
+
+  const removeDuplicateCars = (cars) => {
+    const uniqueVins = new Set(); // Store unique VINs
+    return cars.filter((car) => {
+      if (!car.vin || uniqueVins.has(car.vin)) {
+        return false; // Skip duplicates and empty VINs
+      }
+      uniqueVins.add(car.vin);
+      return true;
+    });
   };
 
   const search = () => {
-    const currentVins = vins.split("\n");
+    const vin = cars.vin.split("\n");
+    const model = cars.model.split("\n");
+    const delivery = cars.delivery.split("\n");
+    const received = cars.received.split("\n");
+    const status = cars.status.split("\n");
 
-    console.log(currentVins);
+    const newCars = [{}];
+    for (let i = 0; i < vin.length; i++) {
+      newCars[i] = {
+        vin: vin[i],
+        model: model[i],
+        delivery: delivery[i],
+        received: received[i],
+        status: status[i],
+      };
+    }
 
-    const duplicateVins = currentVins.filter((vin, index) => {
-      return currentVins.indexOf(vin) != index;
+    setNewCars({
+      ...newCars,
     });
 
-    const duplicateVinsSet = new Set(duplicateVins);
-    const duplicateArray = Array.from(duplicateVinsSet);
+    const uniqueCars = removeDuplicateCars(newCars);
 
-    setResult(duplicateArray);
+    setCarAmount(newCars.length);
+
+    const isExported = uniqueCars.filter((car) => {
+      return car.status == "Ne" || car.status == "";
+    });
+
+    setResult(isExported);
+
+    var v = "";
+    var m = "";
+    var d = "";
+    var r = "";
+    var s = "";
+
+    for (let i = 0; i < isExported.length; i++) {
+      v += isExported[i].vin + "\n";
+      m += isExported[i].model + "\n";
+      d += isExported[i].delivery + "\n";
+      r += isExported[i].received + "\n";
+      s += isExported[i].status + "\n";
+    }
+
+    setResult({
+      ...result,
+      vin: v,
+      model: m,
+      delivery: d,
+      received: r,
+      status: s,
+    });
+
+    setResultAmount(isExported.length);
   };
 
   return (
     <>
       <Paper elevation={12} sx={{ p: 10 }}>
+        <Typography variant="h3" align="center" gutterBottom>
+          Kiekis: {carAmount}
+          {newCars.length}
+        </Typography>
         <Stack>
-          <TextField
-            value={vins}
-            name="vins"
-            label="Įveskite vin numerių sąrašą"
-            placeholder="WAUZZZGY7SA088553 WAUZZZGY9SA088456 WAUZZZGY1SA083204"
-            minRows={10}
-            maxRows={20}
-            multiline
-            helperText="* Kiekvienas vin numeris turi būti atskirtas vienu tarpu"
-            required
-            onChange={onChange}
-          ></TextField>
-          <Button
-            variant="contained"
-            color="success"
-            sx={{ p: 2 }}
-            onClick={search}
+          <Stack direction={"row"} gap={4}>
+            <CustomTextField
+              value={cars?.vin}
+              name="vin"
+              label="Fahrgestellnummer"
+              placeholder="WAUZZZGY7SA088553 ..."
+              onChange={onChange}
+            />
+            <CustomTextField
+              value={cars?.model}
+              name="model"
+              label="Modellname lang"
+              placeholder="Audi A3 Limousine PA MK2 ..."
+              onChange={onChange}
+            />
+            <CustomTextField
+              value={cars.delivery}
+              name="delivery"
+              label="Versandziel"
+              placeholder="DEU 390 B ..."
+              onChange={onChange}
+            />
+            <CustomTextField
+              value={cars.received}
+              name="received"
+              label="Feldzuweisung"
+              placeholder="10.02.2025 08:02:35 ..."
+              onChange={onChange}
+            />
+            <CustomTextField
+              value={cars.status}
+              name="status"
+              label="Būsena"
+              placeholder="Išvežta ..."
+              onChange={onChange}
+            />
+          </Stack>
+          <Box
+            width={"100%"}
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
           >
-            Ieškoti
-          </Button>
+            <Button
+              variant="contained"
+              color="success"
+              sx={{
+                p: 4,
+                m: 4,
+                width: "50%",
+              }}
+              onClick={search}
+            >
+              Ieškoti
+            </Button>
+          </Box>
         </Stack>
       </Paper>
       <Paper elevation={12} sx={{ p: 10 }}>
-        <Stack>
-          <TextField value={result} minRows={10} multiline disabled></TextField>
+        <Typography variant="h3" align="center" gutterBottom>
+          Kiekis: {resultAmount}
+        </Typography>
+        <Stack direction={"row"} gap={4}>
+          <CustomResultTextField label="Fahrgestellnummer" value={result.vin} />
+          <CustomResultTextField label="Modellname lang" value={result.model} />
+          <CustomResultTextField label="Versandziel" value={result.delivery} />
+          <CustomResultTextField
+            label="Feldzuweisung"
+            value={result.received}
+          />
+          <CustomResultTextField label="Būsena" value={result.status} />
         </Stack>
       </Paper>
     </>
