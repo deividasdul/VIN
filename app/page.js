b8a045cb40d2e718e2fa0a264ce4a54e8cc408d9
deividasdul <deividas.dul@gmail.com>
2025-02-12 19:24:23 +0200

    Test

--- app/page.js
+++ app/page.js
@@ -1,3 +1,32 @@
+"use client";
+
+import { Typography, Paper, TextField, Button } from "@mui/material";
+import { useState } from "react";
+
 export default function Home() {
-  return <h1>Test</h1>;
+  const vin = [];
+  const [text, setText] = useState("");
+
+  return (
+    <>
+      <Paper elevation={12}>
+        {/* <Typography variant="h2">VIN</Typography> */}
+        <TextField label="VIN" placeholder="WAUZZZFU8SN018678"></TextField>
+        <Button
+          variant="contained"
+          color="success"
+          onClick={() => {
+            setText("loooooooool");
+          }}
+        >
+          Ieškoti
+        </Button>
+      </Paper>
+      <h1>{text}</h1>
+      {/* <label>VIN: </label>
+      <textarea placeholder="WAUZZZFU8SN018678" rows={10} />
+      <button>Submit</button>
+      <textarea placeholder="WAUZZZFU8SN018678" rows={10} /> */}
+    </>
+  );
 }
